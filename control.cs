49aa179d3429235b67d96a511f8b7b08a0cd636f
dql975166690 <975166690@qq.com>
2018-10-10 15:36:49 +0800

    control.cs提交

---
+++ control.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class control : MonoBehaviour {
+
+    void Start ()
+    {
+        getName();
+
+    }
+	
+	
+	void Update ()
+    {
+		
+	}
+
+    public void getName()
+    {
+        Debug.Log(drop1.captionText.text);
+      if(drop1.captionText.text== "单品套装")
+        {
+            Debug.Log(drop1.captionText.text);
+            drop2.gameObject.SetActive(false);
+            drop6.gameObject.SetActive(false);
+        }
+            
+    }
+
+    public void exit()
+    {
+        return null;
+    }
+
+
+}
